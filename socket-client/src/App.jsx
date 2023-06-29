71eaf7477276e472da789b9e368b1c91931038e5
Rahul Rathore <rathodrahul873@gmail.com>
2023-06-29 22:52:52 +0530

    simple socket to emit messages

--- socket-client/src/App.jsx
+++ socket-client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [recievedMessage, setRecievedMessage] = useState("");
+  const sendMessage = () => {
+    console.log("send message");
+    socket.emit("message", { message: message });
+  };
+  useEffect(() => {
+    sendMessage();
+  }, [message]);
+
+  useEffect(() => {
+    socket.on("receive", (data) => {
+      // console.log(data);
+      setRecievedMessage(data.message);
+    });
+  }, []);
+
+  const handleInput = (e) => {
+    setMessage(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <input placeholder="message" onInput={handleInput} />
+        <button onClick={sendMessage}>Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="message">
+        <h3> {recievedMessage}</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- socket-server/index.js
+++ socket-server/index.js
@@ -1,11 +1,28 @@
+const express = require("express");
+const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
-const io = new Server({
-  /* options */
+const app = express();
+app.use(cors());
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log(`connected with socket id: ${socket.id}`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive", data);
+  });
 });
 
-io.listen(3000);
+server.listen(3001, () => {
+  console.log("listening on Port : 3001");
+});
