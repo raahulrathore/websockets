e875075a1cc9e8d34558b8288b74b8be1d60df22
Rahul Rathore <rathodrahul873@gmail.com>
2023-06-29 23:17:58 +0530

    create rooms instead of 1-1 chat

--- socket-client/src/App.jsx
+++ socket-client/src/App.jsx
@@ -7,8 +7,9 @@ const socket = io("http://localhost:3001");
 function App() {
   const [message, setMessage] = useState("");
   const [recievedMessage, setRecievedMessage] = useState("");
+  const [room, setRoom] = useState("");
   const sendMessage = () => {
-    console.log("send message");
+    // console.log("send message");
     socket.emit("message", { message: message });
   };
   useEffect(() => {
@@ -17,7 +18,7 @@ function App() {
 
   useEffect(() => {
     socket.on("receive", (data) => {
-      // console.log(data);
+      console.log(data);
       setRecievedMessage(data.message);
     });
   }, []);
@@ -26,8 +27,20 @@ function App() {
     setMessage(e.target.value);
   };
 
+  const joinRoom = () => {
+    socket.emit("join_room", room);
+  };
   return (
     <>
+      <div className="room">
+        <input
+          placeholder="room_id"
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join</button>
+      </div>
       <div className="app">
         <input placeholder="message" onInput={handleInput} />
         {/* <button>Send</button> */}
--- socket-server/index.js
+++ socket-server/index.js
@@ -15,12 +15,23 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(`connected with socket id: ${socket.id}`);
+  // console.log(`connected with socket id: ${socket.id}`);
 
-  socket.on("message", (data) => {
-    console.log(data);
-    socket.broadcast.emit("receive", data);
+  //joined room
+  socket.on("join_room", (roomid) => {
+    socket.join(roomid);
+    console.log(`joined room: ${roomid}, socket id: ${socket.id}`);
+    socket.on("message", (data) => {
+      console.log(data, roomid);
+      // socket.broadcast.to(data).emit("receive", data);
+      socket.to(roomid).emit("receive", data);
+    });
   });
+  // socket.on("message", (data) => {
+  // console.log(data);
+  //   socket.broadcast.emit("receive", data);
+  // socket.broadcast.to(data).emit("receive", data);
+  // });
 });
 
 server.listen(3001, () => {
